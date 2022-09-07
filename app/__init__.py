0ea45735ac4b66635bec6dbeb1e6e589a87ae348
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-09-07 20:50:42 -0300

    feat: adicionando cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint
 from flask_restx import Api
+from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app.main.pessoa.pessoa_controller import api as home_ns
 
 app = Flask(__name__)
+CORS(app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 blueprint = Blueprint('api', __name__)
 app.register_blueprint(blueprint)
