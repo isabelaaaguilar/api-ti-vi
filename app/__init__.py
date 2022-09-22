4f7b2e5ee44a6e4b9f24818b5c16a6d7ba4d548d
Kronomant <milardlucas@gmail.com>
2022-09-21 22:40:48 -0300

    feat: upload route

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask, Blueprint
 from flask_restx import Api
+from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
-from app.main.pessoa.pessoa_controller import api as home_ns
+from app.main.pessoa.pessoa_controller import api as home_ns #controller de pessoas
 
 app = Flask(__name__)
+CORS(app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
 blueprint = Blueprint('api', __name__)
 app.register_blueprint(blueprint)
 
 api = Api(app, title='Api Flask Expieriments', version='1.0', description='Api',prefix='/api')
 #adicionado namespace pessoa para rotas
-api.add_namespace(home_ns, path='/pessoa')
+api.add_namespace(home_ns, path='/pessoa') #adiciona rota pessoa
+api.add_namespace(home_ns, path='/teste')
--- app/main/pessoa/pessoa_controller.py
+++ app/main/pessoa/pessoa_controller.py
@@ -1,5 +1,7 @@
+import app
 from flask_restx import Resource, Namespace, fields
-from flask import request
+from flask import request, render_template, redirect
+import os   
 from app.main.pessoa.pessoa_db import PessoaDb
 
 api = Namespace('Pessoa',description='Manutenção dados de pessoa')
@@ -18,3 +20,14 @@ class PessoaController(Resource):
     @api.expect(modelo) #espera modelo ao criar nova pessoa
     def post(self):
         return PessoaDb.adicionar(request.json), 201
+
+@api.route('/upload-image')
+class PessoaUploadController(Resource):
+    def post(self):
+        image = request.files["imgFile"]
+        print(image)
+        image.save(image.filename)
+        
+        
+
+       
