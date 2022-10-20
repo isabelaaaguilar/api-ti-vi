0c0b95accc8bfcfc3e0886cb37836ea544a8a312
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-10-20 11:56:07 -0300

    fix: port change

--- app.py
+++ app.py
@@ -12,4 +12,4 @@ def home():
         
         return jsonify({'success': True, 'message': 'Valid user'})
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=5001, debug=True)
