1a89fe92d001d189d7b9c481d93aaeb3c9b7a994
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-10-19 22:31:37 -0300

    feat: heroku test

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/upload-image", methods=["POST"])
+def home():
+        image = request.files["imgFile"]
+        print(image)
+        image.save(image.filename)
+        
+        return jsonify({'success': True, 'message': 'Valid user'})
+
+app.run(debug=True)
