0f4c54e13825ea406e4e0a88b25e2f8f72084935
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-10-20 12:46:07 -0300

    test

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route("/")
-def home():
+cors = CORS(app)
+
+@app.route("/",methods=['POST'])
+def index():
         image = request.files["imgFile"]
         print(image)
         image.save(image.filename)
         
         return jsonify({'success': True, 'message': 'Valid user'})
 
-app.run()
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="127.0.0.1",port=port, debug=True)
+
+if __name__ == "__main__":
+    main()
