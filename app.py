a348d91258bf5d407cf051207c5728700433271f
isabelaaaguilar <53951492+isabelaaaguilar@users.noreply.github.com>
2022-10-27 10:21:18 -0300

    update app.py

--- app.py
+++ app.py
@@ -5,19 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app)
 
-@app.route("/", methods=["POST", "GET"])
+@app.route("/")
 def home():
-    if request.method == "GET":
         return jsonify({'name':'Jimit',
                     'address':'India'})
-
-    else:
-        file = request.files["image"]
-        upload_image_path = os.path.join(file.filename)
-        print(upload_image_path)
-        file.save(upload_image_path)
-    return jsonify({'label': "Estrela",
-                    'percent': "30%"})
 
 if __name__ == '__main__':
     app.run()
